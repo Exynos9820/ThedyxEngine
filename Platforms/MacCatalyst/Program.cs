5c31c25d1e063f48e70ceaed434733a0f9b14570
Rostyslav <st1tchqwerty@gmail.com>
2024-11-12 23:12:06 +0100

    Test

--- Platforms/MacCatalyst/Program.cs
+++ Platforms/MacCatalyst/Program.cs
@@ -3,13 +3,8 @@ using UIKit;
 
 namespace ThedyxEngine;
 
-public class Program
-{
-    // This is the main entry point of the application.
-    static void Main(string[] args)
-    {
-        // if you want to use a different Application Delegate class from "AppDelegate"
-        // you can specify it here.
+public class Program {
+    static void Main(string[] args) {
         UIApplication.Main(args, null, typeof(AppDelegate));
     }
 }
