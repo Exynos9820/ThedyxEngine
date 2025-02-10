d31a344b9501f788ac69eb3faf1b3793c38db5db
Rostyslav <st1tchqwerty@gmail.com>
2025-02-10 21:35:36 +0100

    Material editor

--- UI/MaterialsList.xaml.cs
+++ UI/MaterialsList.xaml.cs
@@ -3,12 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ThedyxEngine.Engine;
 
 namespace ThedyxEngine.UI;
 
-public partial class MaterialsList : ContentView
-{
+public partial class MaterialsList : ContentView {
+    public Material? _currentSelectedMaterial;
+    public Action<Material>? OnSelectedMaterialChanged;
+
     public MaterialsList() {
         InitializeComponent();
+    }
+
+    public void Update(List<Material> materials) {
+        EngineCollectionView.ItemsSource = null;
+        EngineCollectionView.ItemsSource = materials;
+        if (_currentSelectedMaterial != null && !materials.Contains(_currentSelectedMaterial)) {
+            _currentSelectedMaterial = null;
+        }
+    }
+
+    private void OnSelectionChanged(object? sender, SelectionChangedEventArgs e) {
+        if (e.CurrentSelection.Count <= 0) return;
+        if (e.CurrentSelection[0] is not Material selectedObject) return;
+        _currentSelectedMaterial = selectedObject;
+        OnSelectedMaterialChanged?.Invoke(_currentSelectedMaterial);
     }
 }
--- UI/MaterialsPopup.xaml.cs
+++ UI/MaterialsPopup.xaml.cs
@@ -4,15 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CommunityToolkit.Maui.Views;
+using ThedyxEngine.Engine;
+using ThedyxEngine.Engine.Managers;
 
 namespace ThedyxEngine.UI;
 
 public partial class MaterialsPopup : Popup {
+    private Material? _currentSelectedMaterial;
     public MaterialsPopup() {
         InitializeComponent();
+        
+        // we need to update the list of materials
+        Materials.Update(MaterialManager.GetMaterials());
+        Materials.OnSelectedMaterialChanged += OnSelectedMaterialChanged;
     }
     
+    private void OnSelectedMaterialChanged(Material material) {
+        _currentSelectedMaterial = material;
+        // we need to update the properties of the selected material
+        Name.Text = material.Name;
+    }
+    
+    
     private void OnNameCompleted(object sender, EventArgs e) {
+        // if name is the same, we don't need to update
+        if (_currentSelectedMaterial == null || _currentSelectedMaterial.Name == Name.Text) return;
         
     }
 
